698c7f71411ef123c8f94602ac5e73ed2054ad3b
ransomedesign <info@ransomedesign.co.uk>
2022-01-16 12:40:15 +0000

    m2

---
+++ sensor/house_info.py
@@ -0,0 +1,28 @@
+from datetime import datetime, date
+
+
+class HouseInfo:
+    def __init__(self, data):
+        self.data = data
+
+    def get_data_by_area(self, field, rec_area=0):
+        """
+        The purpose of this method is the filter the data using red_area as the
+        key and field as the value.
+        """
+        field_data = []
+        for record in self.data:
+            if rec_area == 0:
+                field_data.append(record["area"])
+            elif rec_area == int(record["area"]):
+                field_data.append(record[field])
+
+        return field_data
+
+    def get_data_by_date(self, field, rec_date=date.today()):
+        field_data = []
+        for record in self.data:
+            if record["date"] == rec_date.strftime("%m/%d/%y"):
+                field_data.append(record[field])
+
+        return field_data
